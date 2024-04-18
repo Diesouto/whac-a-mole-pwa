0ad2db4e1153463c9b79b5cfb39b9240af045950
Diesouto <diegosouto2000@gmail.com>
2024-04-18 13:35:32 +0200

    refactored switch case

--- src/services/GameService.ts
+++ src/services/GameService.ts
@@ -1,32 +1,25 @@
 class GameService {
-  static generateRandomMolePosition(): number {
-    return Math.floor(Math.random() * 9);
-  }
+  private static readonly difficultySettings: Record<
+    string,
+    { timeInterval: number; pointsIncrement: number }
+  > = {
+    bajo: { timeInterval: 1000, pointsIncrement: 10 },
+    medio: { timeInterval: 750, pointsIncrement: 20 },
+    alto: { timeInterval: 500, pointsIncrement: 30 },
+  };
 
   static getTimeInterval(difficulty: string): number {
-    switch (difficulty) {
-      case "bajo":
-        return 1000;
-      case "medio":
-        return 750;
-      case "alto":
-        return 500;
-      default:
-        return 0;
-    }
+    const settings = this.difficultySettings[difficulty];
+    return settings ? settings.timeInterval : 0;
   }
 
   static getPointsIncrement(difficulty: string): number {
-    switch (difficulty) {
-      case "bajo":
-        return 10;
-      case "medio":
-        return 20;
-      case "alto":
-        return 30;
-      default:
-        return 0;
-    }
+    const settings = this.difficultySettings[difficulty];
+    return settings ? settings.pointsIncrement : 0;
+  }
+
+  static generateRandomMolePosition(): number {
+    return Math.floor(Math.random() * 9);
   }
 }
 
